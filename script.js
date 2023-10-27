99660578cb7533f1461cdbbe7e8e2e14230ad263
Shivam Kumar <118415844+z1shivam@users.noreply.github.com>
2023-10-27 11:55:02 +0530

    new update

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+let clipboard = new ClipboardJS("#copy");
+
 let colorViewer = document.getElementById("colorViewer");
 let colCode = document.getElementById("colCode");
-let clipboard = new ClipboardJS("#copy");
 let generatedColour = "#212121";
 let colorHistoryUL = document.getElementById("colorHistoryUL");
+
+let colorHistory = [];
+if (localStorage.getItem("colorHistory") !== null) {
+  colorHistory = JSON.parse(localStorage.getItem("colorHistory"));
+} else {
+  colorHistory = ["#212121"];
+}
+
+const setAsCurrent = (e) => {
+  colorViewer.style.backgroundColor = e;
+  colCode.textContent = e;
+};
+
+const createElement = (color) => {
+  colorViewer.style.backgroundColor = color[0];
+  colCode.textContent = color[0];
+  colorHistoryUL.innerHTML = "";
+  colorHistory.forEach((color) => {
+    let createdEl = document.createElement("li");
+    createdEl.textContent = color;
+    createdEl.style.backgroundColor = color;
+    colorHistoryUL.appendChild(createdEl);
+  });
+};
+createElement(colorHistory);
 
 let refresh = document
   .getElementById("refresh")
@@ -12,14 +38,22 @@ let refresh = document
     for (let i = 1; i <= 6; i++) {
       generatedColour += hexString.charAt(Math.floor(Math.random() * 16));
     }
-    colorViewer.style.backgroundColor = generatedColour;
-    colCode.innerText = generatedColour;
-
-    let createdEl = document.createElement("li");
-    createdEl.textContent = generatedColour;
-    createdEl.style.backgroundColor = generatedColour;
-    if (colorHistoryUL.children.length >= 9) {
-      colorHistoryUL.removeChild(colorHistoryUL.lastChild);
+    colorHistory.unshift(generatedColour);
+    if (colorHistory.length > 10) {
+      colorHistory.pop();
     }
-    colorHistoryUL.insertBefore(createdEl, colorHistoryUL.firstChild);
+    createElement(colorHistory);
+    localStorage.setItem("colorHistory", JSON.stringify(colorHistory));
+
+    document.querySelectorAll("li").forEach((li) => {
+      li.addEventListener("click", (e) => {
+        setAsCurrent(e.target.textContent);
+      });
+    });
   });
+
+document.getElementById("clearHistory").addEventListener("click", (e) => {
+  colorHistory = ["#212121"];
+  createElement(colorHistory);
+  localStorage.setItem("colorHistory", JSON.stringify(colorHistory));
+});
